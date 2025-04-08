82980f6fcba1617838ab6568cccbe9ce26ab5ada
Sean Reilly <surreily1@gmail.com>
2025-04-08 18:15:45 +0100

    Add animation logic.

--- scripts/player-controller.gd
+++ scripts/player-controller.gd
@@ -1,10 +1,17 @@
 class_name PlayerController extends CharacterBody2D
 
-@export var walk_speed = 200;
-@export var deceleration = 0.1;
-@export var jump_velocity = -400;
+@export var walk_speed = 200
+@export var deceleration = 0.1
+@export var jump_velocity = -400
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity");
+var graphics: Node2D
+var animation: AnimationPlayer
+
+func _ready() -> void:
+	graphics = get_node("Graphics")
+	animation = get_node("Animation")
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	# Gravity.
@@ -18,9 +25,26 @@ func _physics_process(delta: float) -> void:
 	# Horizontal movement.
 	var input_direction = Input.get_axis("left", "right")
 
-	if (input_direction):
-		velocity.x = input_direction * walk_speed;
+	if input_direction:
+		velocity.x = input_direction * walk_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, walk_speed);
+		velocity.x = move_toward(velocity.x, 0, walk_speed)
 
-	move_and_slide();
+	# Animation.
+	if velocity.x > 0.1:
+		graphics.scale.x = 1
+	elif velocity.x < -0.1:
+		graphics.scale.x = -1
+		
+	if velocity:
+		if animation.current_animation != "run":
+			animation.play("RESET")
+			animation.advance(0)
+			animation.play("run")
+	else:
+		if animation.current_animation != "idle":
+			animation.play("RESET")
+			animation.advance(0)
+			animation.play("idle")
+
+	move_and_slide()
